a5850ddbca6be594c98f51b60613f5b2bf32280c
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2016-11-08 15:18:50 -0500

    first commit

---
+++ CompactExposureLamp.cmd
@@ -0,0 +1,17 @@
+# Pin name action command file
+
+# Start of element D3
+ChangePinName(D3, 2, ANODE)
+ChangePinName(D3, 1, CATHODE)
+
+# Start of element D2
+ChangePinName(D2, 2, ANODE)
+ChangePinName(D2, 1, CATHODE)
+
+# Start of element D1
+ChangePinName(D1, 2, ANODE)
+ChangePinName(D1, 1, CATHODE)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 2, 2)
